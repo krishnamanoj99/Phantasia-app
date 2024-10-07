2f8bb3fe1de75757673e2b246f3b3242239fb2dc
krishnamanoj99 <139056352+krishnamanoj99@users.noreply.github.com>
2024-10-07 20:15:57 +0530

    Update Unsplash.js

--- phantasia_frontend/src/api/Unsplash.js
+++ phantasia_frontend/src/api/Unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const unsplash = axios.create({
   baseURL: 'https://unsplash.com/',
   headers: {
-    Authorization: `Client-ID` + process.env.UNSPLASH_API_KEY,
+    Authorization: `Client-ID` + "ybfLEQahubjjWSesXKOJiCyjHYlQobBXfmTwrYT7IBc",
   }
 });
 
